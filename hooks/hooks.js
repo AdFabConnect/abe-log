522a916f681ad4fa657f7aec7cf52e4b694cac53
Christopher Fouquier <christopher.fouquier@gmail.com>
2021-02-22 16:20:19 +0100

    bypass light-date package

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,7 +1,41 @@
 'use strict';
 
 var fs = require('fs');
-var format = require('light-date').format;
+
+/**
+ * Use this API for simple, most common formatting.
+ *
+ * @param {Date}  date - Date object, which should be used.
+ * @param {string} exp - String, which you want to format, for example: `{yyyy}-{MM}-{dd}` or Current time: `{hh}:{mm}:{ss}`.
+ * @return {string} String with formatted date.
+ *
+ * @example
+ * format(new Date(2014, 1, 11), '{yyyy}-{MM}-{dd}') //=> '2014-01-11'
+ */
+function format(date, exp) {
+  return exp.replace(/\\?{.*?}/g, function(key) {
+    if (key.startsWith('\\')) {
+      return key.slice(1);
+    }
+
+    switch (key) {
+      case '{yyyy}':
+        return `${date.getFullYear()}`;
+      case '{MM}':
+        return `${(date.getMonth() + 1)}`.padStart(2, '0');
+      case '{dd}':
+        return `${date.getDate()}`.padStart(2, '0');
+      case '{HH}':
+        return `${date.getHours()}`.padStart(2, '0');
+      case '{mm}':
+        return `${date.getMinutes()}`.padStart(2, '0');
+      case '{ss}':
+        return `${date.getSeconds()}`.padStart(2, '0');
+      default:
+        return '';
+    }
+  });
+}
 
 var hooks = {
   afterDeleteFile: (path, json, abe) => {
